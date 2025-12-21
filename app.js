f4b3216c7ee0b8184cce08cd689466bab25087b3
Ruthless Calm <ruthlesscalm@proton.me>
2025-12-21 08:48:52 +0530

    Add Tailwind CSS setup and create initial HTML template
    - Updated package.json to include Tailwind CSS CLI as a dev dependency and added a script for building styles.
    - Created src/tailwind.css to import Tailwind CSS.
    - Added views/index.html as the initial HTML template for the application with Tailwind CSS integration.

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 import express from "express";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const app = express();
 const PORT = 3000;
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req,res) => {
-    res.send("welcome to Book Store");
+    res.sendFile(path.join(__dirname, "views" , "index.html"));
 })
 
 app.listen(PORT , () => {
